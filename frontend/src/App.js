1bbf5f4921c467829e3970c05b2d7c8af67016dd
KasarapuAnil <anil@xmachines.ai>
2025-07-19 12:55:28 +0530

    dploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Me 🚀 right
+          Learn React with Me 🚀 right ss
         </a>
 
       </header>
